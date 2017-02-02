3d02023229eb41b138ead5d3d370d42040cdd9ea
Marie Filbey <patriciamariefilbey@gmail.com>
2017-02-02 15:38:51 -0500

    Initial commit.

---
+++ main.js
@@ -0,0 +1,8 @@
+//1. Show me how to calculate the average price of all items.
+//A. The average price is $23.63
+
+
+//2.Show me how to get an array of items that cost between $14.00 and $18.00 USD
+//A. 1970s Coors Banquet Glass Beer Pitcher
+//   The Three Broomsticks Customizable Beer Stein Mug, Harry Potter Inspired, hogsmeade village, harry potter gift, three broomsticks mug
+//   Hand Painted Colorful Feather Glass
